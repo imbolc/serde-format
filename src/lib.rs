de4395617e7adf7e7549124a0f0eb0c529c6b4e4
imbolc <me@imbolc.name>
2023-04-13 17:30:34 +0600

    Init

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! # serde-format
+//!
+//! A tiny trait to format a serializable struct using custom placeholders
+
+#![warn(clippy::all, missing_docs, nonstandard_style, future_incompatible)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
